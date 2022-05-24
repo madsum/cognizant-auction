e199fa63326fb7ab3ae4723896131fad1585408f
Masum Islam <masum.islam@dcos.org>
2022-05-24 12:45:04 +0200

    Add user commands

---
+++ seller.cmd.api/src/main/java/com/cognizant/seller/cmd/api/command/RemoveUserCommand.java
@@ -0,0 +1,10 @@
+package com.cognizant.seller.cmd.api.command;
+
+import lombok.Data;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+@Data
+public class RemoveUserCommand {
+    @TargetAggregateIdentifier
+    private String id;
+}
